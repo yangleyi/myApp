af36b42343c25df35c106d56f55ebd331f419dc1
Yangleyi <yangleyi@zhongbiao.mobi>
2021-09-10 17:38:03 +0800

    fixed: 修改文件引入

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import './app.scss'
-import 'taro-ui/dist/style/index.scss'
+// import 'taro-ui/dist/style/index.scss'
+import 'taro-ui/dist/style/components/load-more.scss'
+import 'taro-ui/dist/style/components/search-bar.scss'
+import 'taro-ui/dist/style/components/indexes.scss'
+import 'taro-ui/dist/style/components/list.scss'
+import 'taro-ui/dist/style/components/toast.scss'
 import graphqlClient from './api'
 Vue.prototype.$request = graphqlClient
 import './api/constant'
